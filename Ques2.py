ea6ef0e7ba838d5f177163fb047db0d6cc09fd01
Srinivas Tarun <bittuvalluru@gmail.com>
2023-11-21 21:15:09 -0500

    Completing cleaning of data code

--- Ques2.py
+++ Ques2.py
@@ -24,3 +24,34 @@ def preprocess_and_clean(json_data):
             cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip()
             return cleaned_text
         return None
+    def convert_to_datetime(date_str):
+        if date_str is not None:
+            return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
+        return None
+
+    # Clean and handle null values for top-level attributes
+    json_data['CreatedAt'] = convert_to_datetime(json_data.get('CreatedAt'))
+    json_data['ClosedAt'] = convert_to_datetime(json_data.get('ClosedAt'))
+    json_data['MergedAt'] = convert_to_datetime(json_data.get('MergedAt'))
+    json_data['UpdatedAt'] = convert_to_datetime(json_data.get('UpdatedAt'))
+    json_data['Title'] = clean_text(json_data.get('Title'))
+    json_data['Body'] = clean_text(json_data.get('Body'))
+
+    # Clean and handle null values for nested conversation data
+    if 'ChatgptSharing' in json_data:
+        for convo in json_data['ChatgptSharing']:
+            convo['URL'] = clean_text(convo.get('URL'))
+            convo['Title'] = clean_text(convo.get('Title'))
+            convo['DateOfConversation'] = convert_to_datetime(convo.get('DateOfConversation'))
+
+            if 'Conversations' in convo:
+                convo['Conversations'] = [
+                    {
+                        'Prompt': clean_text(c.get('Prompt')),
+                        'Answer': clean_text(c.get('Answer')),
+                        'ListOfCode': c.get('ListOfCode') if 'ListOfCode' in c else None
+                    }
+                    for c in convo['Conversations']
+                ]
+
+    return json_data
