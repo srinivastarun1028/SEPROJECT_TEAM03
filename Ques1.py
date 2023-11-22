405b2701d7ead19026cef9fbafc3ac26e4d74eaf
Akhil Vuyyuru <av23ba@fsu.edu>
2023-11-21 20:35:43 -0500

    Loading json file

--- Ques1.py
+++ Ques1.py
@@ -1,2 +1,14 @@
 import json
 import matplotlib.pyplot as plt
+
+def load_json_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError as e:
+        print(f"Error: File not found at path {e.filename}")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error: Invalid JSON format in the file {file_path}")
+        return None
