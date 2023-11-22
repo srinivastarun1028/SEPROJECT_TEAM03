57fd0ae5f13a58724dcbf0a8d836db0507f6928f
Akhil Vuyyuru <av23ba@fsu.edu>
2023-11-21 20:39:26 -0500

    creating class for dataprocessing

--- Ques1.py
+++ Ques1.py
@@ -12,3 +12,17 @@ def load_json_file(file_path):
     except json.JSONDecodeError:
         print(f"Error: Invalid JSON format in the file {file_path}")
         return None
+    
+    class DataProcessor:
+    def _init_(self, issues_data, discussions_data):
+        self.issues_data = issues_data
+        self.discussions_data = discussions_data
+
+    def extract_issues_and_discussions(self):
+        resolved_issues = sum(1 for issue in self.issues_data['Sources'] if issue['State'] == 'CLOSED')
+        unresolved_issues = len(self.issues_data['Sources']) - resolved_issues
+
+        resolved_discussions = sum(1 for discussion in self.discussions_data['Sources'] if discussion.get('Closed', False))
+        unresolved_discussions = len(self.discussions_data['Sources']) - resolved_discussions
+
+        return resolved_issues, unresolved_issues, resolved_discussions, unresolved_discussions
